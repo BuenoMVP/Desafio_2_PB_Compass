06219a9bcbd4fef39a4118567a774250c7bdc050
Marcos Bueno <marcos_vbp@outlook.com>
2024-07-29 18:25:13 -0300

    feat: add sidebar menu

--- src/frontend/src/components/GameList.tsx
+++ src/frontend/src/components/GameList.tsx
@@ -1,41 +1,45 @@
 import Game from './Game'
 import './game.css'
 import './gameList.css'
+import '../styles/gameList.css'
+import Menu from './Menu'
 
 const GameList = () => {
   return (
-    
-    <div className="container-game-list">
-      <div className="tbl-container">
-        <div className="tbl-header">
-          <h2>Games List</h2>
-          <button>ADD NEW GAME</button>
+    <section id='game-list-section'>
+      <Menu email='teste@example.com' url='GameList'/>
+      <div className="container-game-list">
+        <div className="tbl-container">
+          <div className="tbl-header">
+            <h2>Games List</h2>
+            <button>ADD NEW GAME</button>
+          </div>
+          <table className="tbl">
+            <thead>
+              <tr>
+                <th></th>
+                <th>Name</th>
+                <th>Description</th>
+                <th>Price</th>
+                <th>Category</th>
+                <th>Date of include</th>
+                <th colSpan={2}></th>
+              </tr>
+            </thead>
+            <tbody>
+              <Game/>
+              <Game/>
+              <Game/>
+              <Game/>
+              <Game/>
+              <Game/>
+              <Game/>
+              <Game/>
+            </tbody>
+          </table>
         </div>
-        <table className="tbl">
-          <thead>
-            <tr>
-              <th></th>
-              <th>Name</th>
-              <th>Description</th>
-              <th>Price</th>
-              <th>Category</th>
-              <th>Date of include</th>
-              <th colSpan={2}></th>
-            </tr>
-          </thead>
-          <tbody>
-            <Game/>
-            <Game/>
-            <Game/>
-            <Game/>
-            <Game/>
-            <Game/>
-            <Game/>
-            <Game/>
-          </tbody>
-        </table>
       </div>
-    </div>
+    </section>
 
   )
 }
--- src/frontend/src/pages/Home.tsx
+++ src/frontend/src/pages/Home.tsx
@@ -1,17 +1,21 @@
-import CardHome from "../components/CardHome"
 import { IoGameController } from "react-icons/io5";
 import { GoTag } from "react-icons/go"
 import "../styles/home.css"
+import CardHome from "../components/CardHome"
+import Menu from "../components/Menu";
 
 const tamIcon = 100
 const totalValue = 150
 
 const Home = () => {
   return (
-    <div className="cardContainer">
-      <CardHome title="Jogos" total={totalValue} icon={<IoGameController size={tamIcon} color="red"/>} />
-      <CardHome title="Genero" total={totalValue} icon={<GoTag size={tamIcon} color="blue"/>} />
-    </div>
+    <section id="home-section">
+      <Menu email="test@email.com" url="Home"/>
+      <div className="cardContainer">
+        <CardHome title="Jogos" total={totalValue} icon={<IoGameController size={tamIcon} color="red"/>} />
+        <CardHome title="Genero" total={totalValue} icon={<GoTag size={tamIcon} color="blue"/>} />
+      </div>
+    </section>
   )
 }
 
