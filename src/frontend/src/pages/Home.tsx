309a919ae7d7e309957d8eb631ce7849380e07d2
Marcos Bueno <marcos_vbp@outlook.com>
2024-07-30 19:29:31 -0300

    feat: add user image in menu

--- src/frontend/src/pages/Home.tsx
+++ src/frontend/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { IoGameController } from "react-icons/io5";
+import { IoGameControllerOutline } from "react-icons/io5";
 import { GoTag } from "react-icons/go"
 import "../styles/home.css"
 import CardHome from "../components/CardHome"
@@ -12,8 +12,8 @@ const Home = () => {
     <section id="home-section">
       <Menu email="test@email.com" url="Home"/>
       <div className="cardContainer">
-        <CardHome title="Jogos" total={totalValue} icon={<IoGameController size={tamIcon} color="red"/>} />
-        <CardHome title="Genero" total={totalValue} icon={<GoTag size={tamIcon} color="blue"/>} />
+        <CardHome title="Jogos" total={totalValue} icon={<IoGameControllerOutline size={tamIcon} color="#74C1ED"/>} />
+        <CardHome title="Genero" total={totalValue} icon={<GoTag size={tamIcon} color="#EE95C5"/>} />
       </div>
     </section>
   )
