234743711d6af9bc3eb94d79f3eb2bcd943e9796
Marcos Bueno <marcos_vbp@outlook.com>
2024-08-02 13:36:39 -0300

    feat: add initial styles on Login page

--- src/frontend/src/pages/Login.tsx
+++ src/frontend/src/pages/Login.tsx
@@ -1,12 +1,10 @@
 import React, { useState, FormEvent } from "react";
 import { Navigate } from "react-router-dom";
-import {
-  doSignWithEmailAndPassword,
-  doSignInWithGoogle,
-} from "../services/auth";
+import { doSignWithEmailAndPassword, doSignInWithGoogle } from "../services/auth";
 import { useAuth } from "../contexts/authContext";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../services/firebase";
+import '../styles/login.css'
 
 const Login: React.FC = () => {
   const [email, setEmail] = useState<string>("");
@@ -54,56 +52,58 @@ const Login: React.FC = () => {
   };
 
   return (
-    <div className="signInContainer">
-      {userLoggedIn && <Navigate to="/Home" replace={true} />}
-      <h1>CRUD OPERATIONS</h1>
-      <form onSubmit={isSigning ? handleRegister : handleSignIn}>
-        <h2>{isSigning ? "SIGN UP" : "SIGN IN"}</h2>
-        <p>
-          {isSigning
-            ? "Create your account"
-            : "Enter your credentials to acess your account"}
-        </p>
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          placeholder="Enter your email"
-          className="inputBase"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <span className="text-red-600 font bold">Invalid Email</span>
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          placeholder="Enter your password"
-          className="inputBase"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <span className="text-red-600 font bold">Invalid Password</span>
-        <button type="submit" className="signInButton" onClick={() => window.location.href = "/Home"}>
-          Sign In
-        </button>
-        <a
-          className="signUpA"
-          onClick={(e) => {
-            e.preventDefault();
-            setIsSignin(true);
-          }}
-        >
-          Sign Up
-        </a>
-        <button
-          disabled={isSigning}
-          onClick={onGoogleSignIn}
-          className="googleButton"
-        >
-          Sign with Google
-        </button>
-        {errorMessage && <p className="error">{errorMessage}</p>}
-      </form>
-    </div>
+    <section id="login-section">
+      <div className="signInContainer">
+        {userLoggedIn && <Navigate to="/Home" replace={true} />}
+        <h1>CRUD OPERATIONS</h1>
+        <form id="login-form" onSubmit={isSigning ? handleRegister : handleSignIn}>
+          <h2>{isSigning ? "SIGN UP" : "SIGN IN"}</h2>
+          <p>
+            {isSigning
+              ? "Create your account"
+              : "Enter your credentials to acess your account"}
+          </p>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            placeholder="Enter your email"
+            className="inputBase"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <span className="text-red-600 font bold">Invalid Email</span>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            placeholder="Enter your password"
+            className="inputBase"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <span className="text-red-600 font bold">Invalid Password</span>
+          <button type="submit" className="signInButton" onClick={() => window.location.href = "/Home"}>
+            Sign In
+          </button>
+          <a
+            className="signUpA"
+            onClick={(e) => {
+              e.preventDefault();
+              setIsSignin(true);
+            }}
+          >
+            Sign Up
+          </a>
+          <button
+            disabled={isSigning}
+            onClick={onGoogleSignIn}
+            className="googleButton"
+          >
+            Sign with Google
+          </button>
+          {errorMessage && <p className="error">{errorMessage}</p>}
+        </form>
+      </div>
+    </section>
   );
 };
 
