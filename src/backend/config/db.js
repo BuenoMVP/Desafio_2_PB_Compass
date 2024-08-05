ff9851fb50ae682c1842577f2cdd1bde006909d2
Marcos Bueno <marcos_vbp@outlook.com>
2024-08-05 15:40:59 -0300

    docs: add db response

--- src/backend/config/db.js
+++ src/backend/config/db.js
@@ -8,9 +8,12 @@ const connectDB = async () => {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
+        console.log('MongoDB connected');
     } catch (error) {
+        console.error('Error connecting to MongoDB', error)
         process.exit(1)
     }
 }
+
 
 module.exports = connectDB
